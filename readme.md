dcafc7da778391a514f29d36b29b5015328ac1fb
Alex Huang <Alex_Huang@external.mckinsey.com>
2022-05-31 14:47:50 +0800

    Add files via upload

--- readme.md
+++ readme.md
@@ -9,28 +9,10 @@
 ```shell
 conda create --name stitcher python=3.6.9
 conda activate stitcher
-conda install opencv=3.4.2
-conda install opencv-contrib-python=3.4.2
-conda install numpy
+python -m pip install opencv-python==4.2.0.34 
+python -m pip install opencv-contrib-python==3.4.2.17
+python -m pip install numpy
 ```
-
-
-
-## Usage
-
-```shell
-python stitching.py --dir [IMAGE_DIRECTORY] --row [NUMBER_OF_ROWS]
-
-Example:
-python stitching.py --dir ./test_images/
-python stitching.py --dir ./test_images/ --row 2
-```
-
-> `--dir [IMAGE_DIRECTORY]` &rarr; image folder name.
->
-> `--row [NUMBER_OF_ROWS]` &rarr; This is optional. If `--row [NUMBER_OF_ROWS]` is given, the different algorithm will be triggered. The stitcher will stitch images for each row first, and then stitch them vertically.
->
-> **The stitched result will be save in `[IMAGE_DIRECTORY]/Result/`**
 
 
 
@@ -38,8 +20,62 @@ python stitching.py --dir ./test_images/ --row 2
 
 1. Choose **2x mode** to take picture
 
-    <img src="https://github.com/fingerk28/Image-Stitcher/blob/master/pic.png" alt="Picture1" style="zoom: 20%;" />
+    <img src="C:\Users\Alex Huang\OneDrive - McKinsey & Company\Desktop\pic.png" alt="Picture1" style="zoom: 20%;" />
 
-2. Keep an appropriate distance between your iPhone and PCB and allow the text on IC can be clearly capture and FOV is as large as possible.
-3. If the PCB is too large and can not be capture in one row, take pictures in **Z shape**.
+2. Keep an appropriate distance between your iPhone and PCB and allow the text on IC can be clearly identified and FOV is as large as possible.
+
+3. Please take pictures **from left to right**.
+
+4. If the PCB is too large and can not be capture in one row, please take pictures in **Z shape**. (from left to right in the first row, from right to left in the second row)
+
 4. The overlapping between photos should be in **20~30%**.
+
+
+
+## Usage
+
+```shell
+python stitching.py --dir [IMAGE_DIRECTORY] --row [NUMBER_OF_ROWS] --result_dir [RESULT_DIR]
+
+Example:
+python stitching.py --dir ./test_images/
+python stitching.py --dir ./test_images/ --row 2
+python stitching.py --dir ./test_images/ --result_dir result_directory
+```
+
+> `--dir [IMAGE_DIRECTORY]` &rarr; image folder name.
+>
+> `--row [NUMBER_OF_ROWS]` &rarr; This is optional. If the PCB is too large such that you need to take pictures in many rows, you can choose whether to give this argument. In default setting, you don't need to input this argument. If `--row [NUMBER_OF_ROWS]` is given, the different algorithm will be triggered. The stitcher will stitch images for each row first (horizontally), and then stitch them vertically.
+>
+> `--result_dir [RESULT_DIR]` &rarr; This is optional. **The stitched result and log file will be save in `[IMAGE_DIRECTORY]/[RESULT_DIR]/`**. Default value is `Result`
+>
+> ---
+>
+> The status return by `stitcher.fit()`
+>
+> * 0 &rarr; successfully
+> * -1 &rarr; fail
+>
+> The stitching process contains randomness. If the `stitcher.fit()` return -1 in the first trial, you can try again and it may success.
+>
+> ---
+>
+> As a reference, the execution time is as follows (image resolution is **3024x4032**)
+>
+> * 4 images &rarr; **4~5s**
+>
+> * 6 images &rarr; **5~6s**
+>
+> * 8 images &rarr; **8~10s**
+>
+> However, the running time depends on the difficulty of stitching.
+>
+> 1. Don't use the **2x mode** to take photos
+> 2. The distance between iPhone and PCB is too close (FOV is too small)
+> 3. The overlapping is too small or too large
+>
+> If the above conditions happen, the execution time may be as follows
+>
+> * 8 images &rarr; **35s**
+> * 10 images &rarr; **60s** 
+
--- stitching.py
+++ stitching.py
@@ -1,30 +1,67 @@
+import logging
 import cv2
 import numpy as np
 from glob import glob
 import os
 import time
 import argparse
+import logging
+import datetime
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--dir', type=str)
 parser.add_argument('--row', type=int, default=1)
 parser.add_argument('--col', type=int)
+parser.add_argument('--result_dir', type=str, default='Result')
 args = parser.parse_args()
 
 
 class Stitcher():
-    def __init__(self, args,) :
-        print('-----------------------------------')
-        print('[INITIALIZING]')
+    def __init__(self, args,):
+        self.save_dir = self.prepare_result_dir(args)
+        self.logger = self.prepare_logger()
+        self.logger.info('[INITIALIZING]')
         self.args = args
         self.elapsed_time = []
         self.stitcher = cv2.createStitcher(False)
-        print('-----------------------------------')
+        self.set_divider()
+        self.logger.info(self.long_divider)
+
+    def set_divider(self):
+        self.long_divider = '--------------------------------------------------'
+        self.short_divider = '----------------------'
+
+    def prepare_result_dir(self, args):
+        save_dir = os.path.join(args.dir, args.result_dir)
+        os.makedirs(save_dir, exist_ok=True)
+        
+        return save_dir
+
+    def prepare_logger(self):
+        logger = logging.getLogger()
+        logger.setLevel(logging.DEBUG)
+        formatter = logging.Formatter(
+            '[%(levelname)1.1s %(asctime)s %(module)s:%(lineno)d] %(message)s',
+            datefmt='%Y%m%d %H:%M:%S')
+
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
+        ch.setFormatter(formatter)
+        
+        log_filename = datetime.datetime.now().strftime(os.path.join(self.save_dir, "%Y-%m-%d_%H_%M_%S.log"))
+        fh = logging.FileHandler(log_filename)
+        fh.setLevel(logging.DEBUG)
+        fh.setFormatter(formatter)
+
+        logger.addHandler(ch)
+        logger.addHandler(fh)
+
+        return logger
 
     def prepare_image_paths(self):
-        print('[PREPARING IMAGE PATHS]')
-        print('------------------')
+        self.logger.info('[PREPARING IMAGE PATHS]')
+        self.logger.info(self.short_divider)
         source_paths = [path for path in glob(os.path.join(self.args.dir, '*')) if os.path.isfile(path)]
         source_paths = sorted(source_paths, key=lambda s: os.path.splitext(os.path.basename(s))[0])
 
@@ -38,54 +75,65 @@ class Stitcher():
             else:
                 img_paths.append(source_paths[i*args.col: (i+1)*args.col])
 
-        print('Number of images: {}'.format(self.args.row * self.args.col))
-        print('Number of rows / cols: {} / {}'.format(self.args.row, self.args.col))
-        print('------------------')
-        print('Image paths:')
+        self.logger.info('Number of images: {}'.format(self.args.row * self.args.col))
+        self.logger.info('Number of rows / cols: {} / {}'.format(self.args.row, self.args.col))
+        self.logger.info(self.short_divider)
+        self.logger.info('Image paths:')
         for paths in img_paths:
-            print(paths)
+            self.logger.info(paths)
 
-        print('-----------------------------------')
+        self.logger.info(self.long_divider)
         return img_paths
 
     def read_images(self):
-        print('[READING IMAGES]')
+        self.logger.info('[READING IMAGES]')
         images = []
         for i in range(self.args.row):
             images_one_row = [cv2.imread(self.image_paths[i][j]) for j in range(self.args.col)]
             images.append(images_one_row)
-        print('-----------------------------------')
+        self.logger.info(self.long_divider)
 
         return images
 
     def cv2_stitch(self, images):
         self.log_time(flag='start')
-        (_, pano) = self.stitcher.stitch(images)
+        (status, pano) = self.stitcher.stitch(images)
         self.log_time(flag='end')
-        self.print_elapsed_time()
-        return pano
+        self.log_elapsed_time()
+
+        if status == 1:
+            self.logger.info('ERR_NEED_MORE_IMGS')
+        elif status == 2:
+            self.logger.info('ERR_HOMOGRAPHY_EST_FAIL')
+        elif status == 3:
+            self.logger.info('ERR_CAMERA_PARAMS_ADJUST_FAIL')
+        
+        return status, pano
 
     def stitch(self, images):
-        print('[STARTING TO STITCH IMAGES HORIZONTALLY]')
+        self.logger.info('[STARTING TO STITCH IMAGES HORIZONTALLY]')
         stitched_images = []
         for i in range(self.args.row):
-            print('------------------')
-            print('Stitching row {} images...'.format(i))
-            stitched_images.append(self.cv2_stitch(images[i]))
-        print('-----------------------------------')
+            self.logger.info(self.short_divider)
+            self.logger.info('Stitching row {} images...'.format(i))
+            status, result = self.cv2_stitch(images[i])
+            if status != 0:
+                return -1, None
+            stitched_images.append(result)
+        self.logger.info(self.long_divider)
 
         if self.args.row > 1:
             self.save_per_row_image(stitched_images)
-            print('[STARTING TO STITCH IMAGES VERTICALLY]')
+            self.logger.info('[STARTING TO STITCH IMAGES VERTICALLY]')
             stitched_images = [np.rot90(image) for image in stitched_images]
             stitched_images = self.cv2_stitch(stitched_images)
             stitched_images = np.rot90(stitched_images, k=-1)
         else:
-            print('[NOTES] no need to stitch images vertically')
+            self.logger.info('[NOTES] no need to stitch images vertically')
             stitched_images = stitched_images[0]
-        print('-----------------------------------')
+        self.logger.info(self.long_divider)
 
-        return stitched_images
+        return status, stitched_images
 
     def log_time(self, flag):
         if flag == 'start':
@@ -93,38 +141,32 @@ class Stitcher():
         else:
             self.end_time = time.time()
     
-    def print_elapsed_time(self):
+    def log_elapsed_time(self):
         elapsed_time = self.end_time - self.start_time
         self.elapsed_time.append(elapsed_time)
-        print('Elapsed time: {:.3f}'.format(elapsed_time))
+        self.logger.info('Elapsed time: {:.3f}'.format(elapsed_time))
 
         return elapsed_time
 
-    def print_avg_elapsed_time(self):
-        print('Average Elapsed time: {:.3f}'.format(np.mean(self.elapsed_time)))
-        print('-----------------------------------')
+    def log_avg_elapsed_time(self):
+        self.logger.info('Average Elapsed time: {:.3f}'.format(np.mean(self.elapsed_time)))
+        self.logger.info(self.long_divider)
 
     def save_stitched_image(self, stitched_image):
-        save_dir = os.path.join(self.args.dir, 'Result')
-        os.makedirs(save_dir, exist_ok=True)
-        cv2.imwrite(os.path.join(save_dir, 'result.png'), stitched_image)
-        print('Save stitched image...')
-        print('-----------------------------------')
+        cv2.imwrite(os.path.join(self.save_dir, 'result.png'), stitched_image)
+        self.logger.info('Save stitched image...')
+        self.logger.info(self.long_divider)
     
     def save_rectified_image(self, image):
-        save_dir = os.path.join(self.args.dir, 'Result')
-        os.makedirs(save_dir, exist_ok=True)
-        cv2.imwrite(os.path.join(save_dir, 'rectified_result.png'), image)
-        print('Save rectified image...')
-        print('-----------------------------------')
+        cv2.imwrite(os.path.join(self.save_dir, 'rectified_result.png'), image)
+        self.logger.info('Save rectified image...')
+        self.logger.info(self.long_divider)
 
     def save_per_row_image(self, images):
-        save_dir = os.path.join(self.args.dir, 'Result')
-        os.makedirs(save_dir, exist_ok=True)
         for i in range(len(images)):
-            cv2.imwrite(os.path.join(save_dir, 'row{}_result.png'.format(i)), images[i])
-        print('Save per row image...')
-        print('-----------------------------------')
+            cv2.imwrite(os.path.join(self.save_dir, 'row{}_result.png'.format(i)), images[i])
+        self.logger.info('Save per row image...')
+        self.logger.info(self.long_divider)
 
     def rectify(self, image):
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -135,7 +177,7 @@ class Stitcher():
 
         contours = sorted(contours, key=lambda c: cv2.contourArea(c), reverse=True)
         max_contour = contours[1]
-        print('Max area: {}'.format(cv2.contourArea(max_contour)))
+        self.logger.info('Max area: {}'.format(cv2.contourArea(max_contour)))
 
         x, y, w, h = cv2.boundingRect(max_contour)
 
@@ -144,23 +186,26 @@ class Stitcher():
     def fit(self):
         self.image_paths = self.prepare_image_paths()
         self.images = self.read_images()
-        stitched_image = self.stitch(self.images)
-        self.save_stitched_image(stitched_image)
-        self.print_avg_elapsed_time()
-        rectified_image = self.rectify(stitched_image)
-        self.save_rectified_image(rectified_image)
+        status, stitched_image = self.stitch(self.images)
+        if status == 0:
+            self.save_stitched_image(stitched_image)
+            self.log_avg_elapsed_time()
+            rectified_image = self.rectify(stitched_image)
+            self.save_rectified_image(rectified_image)
+            self.logger.info('Stitch images successfully')
+        else:
+            self.logger.info('Stitch images fail')
+
+        return status
 
 
 def main():
     
     stitcher = Stitcher(args)
-    stitcher.fit()
+    status = stitcher.fit()
 
 
 if __name__ == '__main__':
-    try: 
-        main()
+    main()
 
-    except:
-        print ("Something wrong...")
     
